8ad1337c6a08823c97ea4fde2bd696705c18eccc
Lindita Morina <linditamorinna@gmail.com>
2026-03-24 12:08:44 +0100

    initial: project setup with secure gitignore and env template

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import QueryProvider from '../providers/QueryProvider';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
+    </html>
+  );
+}
